e2d9a280de461a36b4a91d3b9deb5d24df0c541d
davidroulet <47849716+davidroulet@users.noreply.github.com>
2019-03-07 15:46:59 +0100

    Intro Prog

---
+++ BN/cmake-build-debug/CMakeFiles/BN.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BN.dir/main.c.obj"
+  "BN.pdb"
+  "BN.exe"
+  "BN.exe.manifest"
+  "libBN.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BN.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
