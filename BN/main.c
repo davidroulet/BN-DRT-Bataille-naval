4b84a6efd0e8f1679e7a73debf81547b4b811a24
davidroulet <47849716+davidroulet@users.noreply.github.com>
2019-03-22 09:35:07 +0100

    Code Grille !

--- BN/main.c
+++ BN/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <windows.h>
 
-int const Taille =    8    -1;
+int const Taille = 8 - 1;
 #define DTLC 201 // ╔, Double Top Left Corner
 #define DTRC 187 // ╗, Double Top Right Corner
 #define DBLC 200 // ╚, Double Bottom Left Corner
@@ -15,107 +15,118 @@ int const Taille =    8    -1;
 #define DC   206 // ╬, Double Center
 #pragma execution_character_set("utf-8")
 
-void GrilleTop() {
-    printf("%c%c%c%c", DHSB,DHSB, DHSB, DHTB);
-}
+void Grille() {
+ //   int B1=3;
+ //   int DonneeC[8][8] = {
+ //           {B1, 0, 0, 0, 0, 0, 0, 0},
+ //           {B1, 0, 0, 0, 0, 0, 0, 0},
+ //           {B1, 0, 0, 0, 0, 0, 0, 0},
+ //           {0, 0, 0, 0, 0, 0, 0, 0},
+ //           {0, 0, 0, 0, 0, 0, 0, 0},
+//            {0, 0, 0, 0, 0, 0, 0, 0},
+ //           {0, 0, 0, 0, 0, 0, 0, 0},
+ //           {0, 0, 0, 0, 0, 0, 0, 0},
+//
+ //   };
+    int Donnee[8][8] = {
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
 
-void Grille2() {
+    };
+    SetConsoleOutputCP(437); // For semi-graphic characters
+    int Lingne = 0;
+    int Colonne = 0;
+    int Num = 1;
+    printf("  A   B   C   D   E   F   G   H\n");
+    printf(" ");
+    printf("%c", DTLC);
+    for (int i = 0; i < Taille; ++i) {
+
+        printf("%c%c%c%c", DHSB, DHSB, DHSB, DHTB);
+    }
+    printf("%c%c%c%c\n", 205, DHSB, 205, DTRC);
+
+
+    for (int l = 0; l < Taille; ++l) {
+
+        printf("%d", Num);
+        Num++;
+        for (int j = 0; j < Taille + 1; ++j) {
+
+            printf("%c", 186);
+            printf(" %c ", Donnee[Colonne][Lingne]);
+            Lingne++;
+            if (Lingne > 7) {
+                Lingne = 0;
+                Colonne++;
+            }
+        }
+        printf("%c", 186);
+        printf("\n");
+        printf(" ");
+        printf("%c", 204);
+
+        for (int k = 0; k < Taille; ++k) {
+            printf("%c%c%c%c", 205, 205, 205, DC);
+        }
+
+        printf("%c%c%c%c\n", 205, 205, 205, 185);
+
+    }
+    printf("%d", Num);
+    for (int j = 0; j < Taille + 1; ++j) {
+        printf("%c", 186);
+
+        printf(" %c ", Donnee[Colonne][Lingne]);
+        Lingne++;
+        if (Lingne > 7) {
+            Lingne = 0;
+            Colonne++;
+        }
+    }
+
     printf("%c", 186);
-}
+    printf("\n %c", 200);
 
-void GrilleMid() {
-    printf("%c%c%c%c",205, 205, 205, DC);
-}
-void GrilleDown(){
-    printf("%c%c%c%c", DHSB,DHSB, DHSB, DHBB);
+    for (int i = 0; i < Taille; ++i) {
+        printf("%c%c%c%c", DHSB, DHSB, DHSB, DHBB);
+    }
+    printf("%c%c%c%c", 205, 205, 205, 188);
 }
 
 int main() {
-    int Test=49;
-    int Num=49;
+
     SetConsoleOutputCP(65001);
     int reponce;
-    printf("Bonjour Welcome to Bataille naval\n Donner un chiffre pour choisir une action \n1.\tJeux\n2.\tAide\n");
-
+    printf("Bonjour Welcome to Bataille navale\n ");
+    printf("un chiffre pour choisir une action \n1.\tJeux\n2.\tAide\n");
     scanf("%d", &reponce);
     while (reponce != 2 && reponce != 1) {
-        printf("un nombre entre 1 et 2 Stp");
+        printf("Un nombre entre 1 et 2 Stp");
         scanf("%d", &reponce);
     }
-
     if (reponce == 2)
-        printf("Comment Jouer ?\n Alors le princpe et simple il ce joue sur un plateau il vas avoir des bateau addvers que vous aller devoir touché pour cela vous allez devoir choisir une case ou tiré si il y as un bateau a cette endroit cela affichera touché et si il y as de l eau cela affichera a l'eau l objetif est de touché tout les bateaus une fois que vous avez touché tout les bateaus vous avez ganger la partie \n");
+        printf("Comment Jouer ?\n Alors le principe est simple il se joue sur un plateau il va y avoir des bateaux adverses que vous allez devoir toucher. Pour cela vous allez devoir choisir une case où tirer, si il y à un bateau à cet endroit cela affichera touché et si il y a de l'eau cela affichera à l'eau l'objetif est de touché tout les bateaux. Une fois que vous avez coulé tout les bateaux, vous avez gagné la partie. \n");
     else {
-
         SetConsoleOutputCP(437); // For semi-graphic characters
-        
-        printf("  A   B   C   D   E   F   G   H\n"
-               "");
-        printf(" ");
-        printf("%c", DTLC);
-        for (int i = 0; i < Taille; ++i) {
-
-            GrilleTop();
-        }
-        printf("%c%c%c%c\n", 205,DHSB, 205, DTRC);
-
-
-        for (int l = 0; l <Taille ; ++l) {
-
-            printf("%c",Num);
-            Num++;
-            for (int j = 0; j < Taille + 1; ++j) {
-
-                Grille2();
-                printf(" %c ",Test);
-                Test++;
-            }
-            printf("%c",186);
-            printf("\n");
-            printf(" ");
-            printf("%c", 204);
-
-            for (int k = 0; k < Taille; ++k) {
-                GrilleMid();
-            }
-
-            printf("%c%c%c%c\n", 205,205, 205, 185);
-
-        }
-        printf("%c",Num);
-        Num++;
-        for (int j = 0; j < Taille + 1; ++j) {
-            Grille2();
-            printf(" %c ",Test);
-            Test++;
-        }
-
-        printf("%c",186);
-        printf("\n %c", 200);
-
-        for (int i = 0; i < Taille; ++i) {
-            GrilleDown();
-        }
-        printf("%c%c%c%c",205,205,205,188);
+        Grille();
     }
+    printf("\n");
+    printf("Dans qulle Ligne Voulez Vous Tiré");
+    int Choix=0;
+    scanf("%d",&Choix);
 
 
     system("pause");
     system("pause");
     system("cls");
-    system("pause");
 
 
     return 0;
 }
-/*
- * for (int k = 0; k < Taille; ++k) {
-GrilleMid();
-}
-    printf("%c%c%c", 205, 205, 185);
- *
- *   printf("\n%c", 204);
-
- *
- *
- */
